a2a5f523a766afb59568c1bb6e3aac1c5c8c87e0
GeorgeHN666 <89957301+GeorgeHN666@users.noreply.github.com>
2023-11-13 03:53:41 -0600

    adding files

---
+++ _next/static/chunks/app/layout-0c3c5ec9e522b813.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6463:function(e,n,t){Promise.resolve().then(t.t.bind(t,8326,23)),Promise.resolve().then(t.t.bind(t,2282,23)),Promise.resolve().then(t.t.bind(t,595,23))},595:function(){}},function(e){e.O(0,[518,971,472,744],function(){return e(e.s=6463)}),_N_E=e.O()}]);
